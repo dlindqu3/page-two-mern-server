24102303cf88ff8ad719fef1dade86ce32262976
Dwight Lindquist <dwight.lindquist@gmail.com>
2022-08-16 10:18:56 -0400

    connected to mongo

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-require('dotenv').config()
+require('dotenv').config();
 const express = require('express'); 
+const mongoose = require('mongoose'); 
 const nyt_bestsellers_routes = require('./routes/nyt_bestsellers'); 
 
 const app = express(); 
@@ -12,6 +13,15 @@ app.use((req, res, next) => {
 // routes
 app.use('/api/nyt-bestsellers', nyt_bestsellers_routes)
 
-app.listen(process.env.PORT, () => {
-  console.log(`listening on port ${process.env.PORT}`)
-})
+// connect to db
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(process.env.MONGO_URI)
+      console.log(`connected to mongoDB and listening on port ${process.env.PORT}`)
+    })
+  })
+  .catch((error) => {
+    console.log(error)
+  })
+
